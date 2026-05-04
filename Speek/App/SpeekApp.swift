d9cd13fa9d6fc1a46307c18d0d8f497bc70dac49
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2026-05-03 22:23:35 -0700

    scaffold: xcodegen project with menu-bar accessory mode

---
+++ Speek/App/SpeekApp.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import AppKit
+
+@main
+struct SpeekApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        Settings {
+            SettingsView()
+        }
+    }
+}
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+    }
+}
---
+++ SpeekTests/SpeekTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Speek
+
+final class SpeekTests: XCTestCase {
+    func test_baseline_truth() {
+        XCTAssertTrue(true)
+    }
+}
